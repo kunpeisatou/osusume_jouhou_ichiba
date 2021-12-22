18c257f3a8b78534975bfa1ee29394c739d35336
kunpeisatou <kunpei0411@gmail.com>
2021-12-22 10:11:03 +0000

    fix顧客サインアップ

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -52,11 +52,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    categories_path
+    root_path
   end
   
   def after_update_path_for(resource)
-    categories_path
+    root_path
   end
 
   # The path used after sign up for inactive accounts.
