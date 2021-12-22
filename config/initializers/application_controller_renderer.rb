894f2c350dcad4b5a2d28f0e21451727c24e0462
kunpeisatou <kunpei0411@gmail.com>
2021-12-22 07:20:45 +0000

    fix画像投稿機能config/initial/app編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0faf945a55ebb87766cd2e3ffdd45db43eca925d19764cbc85d4f8c02d27d8ab1d3dc6bb1ecbed4628b98d4c77aaec11af755540843b899e640d0469cc90a99c'
