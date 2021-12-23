92d5379c1bd38b7af5bb0e08f37b99e4754b7783
kunpeisatou <kunpei0411@gmail.com>
2021-12-23 06:46:04 +0000

    addいいね機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,12 @@ class Item < ApplicationRecord
    belongs_to :category
    attachment :image
    
+   has_many :favorites, dependent: :destroy
+
+   def favorited_by?(customer)
+     favorites.where(customer_id: customer.id).exists?
+   end
+   
    validates :name, presence: true
    validates :introduction, presence: true
    validates :category_id, presence: true
