25df0c1f960ae3b723455021a0a6815473ca285d
kunpeisatou <kunpei0411@gmail.com>
2021-12-24 07:28:00 +0000

    add 新着情報

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -2,6 +2,7 @@ class Public::CategoriesController < ApplicationController
   
   def index
     @categories = Category.page(params[:page]).reverse_order
+    @items = Item.order('id DESC').limit(5)
   end
   
   def show
